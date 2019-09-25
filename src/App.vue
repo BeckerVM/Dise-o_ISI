3b27927c06f109e2942ce8b2c2f96a0187528ef8
unknown <beiker2203@gmail.com>
2019-09-25 01:19:50 -0500

    Avanzando el diseño de la pagina de inicio

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <header class="header">
+      <div class="navigation">
+        <div class="navigation__superior">
+          <i class="fas fa-bars navigation__icon"></i>
+          <img  class="navigation__logo" src="./assets/images/logo.png" alt="Logo">
+          <h3 class="navigation__title">
+            <span class="navigation__text">Ingeniería de Sistemas</span>
+            <span class="navigation__text">e Informática</span>
+          </h3>
+          
+        </div>
+        <nav class="navigation__menu">
+          <a class="navigation__link active" href="#">Inicio</a>
+          <a class="navigation__link" href="#">Nosotros</a>
+          <a class="navigation__link" href="#">Proyectos</a>
+          <a class="navigation__link" href="#">Eventos</a>
+          <a class="navigation__link" href="#">Laboratorios</a>
+          <a class="navigation__link" href="#">Noticias</a>
+          <a class="navigation__link" href="#">Docentes</a>
+        </nav>
+      </div>
+    </header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+@import "./assets/sass/main.scss";
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="home">
+    <div class="home__video">
+      
+    </div>
+    <div class="home__mvp">
+      <div class="home__mvp-col">
+        <h3 class="title">Mision y Vision</h3>
+        <div class="home__mvp-container-options">
+          <div class="home__mvp-options">
+            <span class="active">Mision</span>
+            <span>Vision</span>
+            <span>Perfil</span>
+          </div>
+          <div class="home__mvp-text">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, 
+              velit fugiat! Illo nihil deserunt repellat vero asperiores vitae modi tempora alias quibusdam exercitationem, 
+              veritatis dignissimos soluta at accusantium sapiente eaque!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="home__mvp-col">
+        <div class="home__mvp-container-logo">
+          <div class="home__mvp-logo">
+            <img src="../assets/images/logo.png" alt="Logo">
+            <div class="home__mvp-isi">
+              <span>I</span>
+              <span>S</span>
+              <span>I</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="home__projects">
+
+    </div>
+    <div class="home__events">
+      <h3 class="title">Nuestros Eventos</h3>
+      <div class="home__slider">
+        <div class="home__slider-item">
+          <div class="home__slider-calendar">
+            <span>Lunes</span>
+            <div>
+              <span>26</span>
+              <span>AGOSTO</span>
+            </div>
+          </div>
+          <div class="home__slider-text">
+            <h4>Bienvenida de cachimbos 2019</h4>
+            <span>Presencial</span>
+            <span>10:30 a.m - 5:00 p.m</span>
+            <a class="home__slider-link" href="#">MAS SOBRE EL EVENTO</a>
+          </div>
+        </div>
+         <div class="home__slider-item">
+          <div class="home__slider-calendar">
+            <span>Lunes</span>
+            <div>
+              <span>26</span>
+              <span>AGOSTO</span>
+            </div>
+          </div>
+          <div class="home__slider-text">
+            <h4>Bienvenida de cachimbos 2019</h4>
+            <span>Presencial</span>
+            <span>10:30 a.m - 5:00 p.m</span>
+            <a class="home__slider-link" href="#">MAS SOBRE EL EVENTO</a>
+          </div>
+        </div>
+         <div class="home__slider-item">
+          <div class="home__slider-calendar">
+            <span>Lunes</span>
+            <div>
+              <span>26</span>
+              <span>AGOSTO</span>
+            </div>
+          </div>
+          <div class="home__slider-text">
+            <h4>Bienvenida de cachimbos 2019</h4>
+            <span>Presencial</span>
+            <span>10:30 a.m - 5:00 p.m</span>
+            <a href="#" class="home__slider-link">MAS SOBRE EL EVENTO</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="home__labs">
+      <h3 class="title">Nuestros Laboratorios</h3>
+      <div class="home__labs-content">
+        <div class="home__labs-col">
+          <div class="home__labs-types">
+            <div class="home__labs-type">
+              <span>I</span>
+              <span>N</span>
+              <span>F</span>
+              <span>O</span>
+              <span>R</span>
+              <!--<span>M</span>
+              <span>A</span>
+              <span>T</span>
+              <span>I</span>
+              <span>C</span>
+              <span>A</span>-->
+            </div>
+            <div class="home__labs-type active">
+              <span>R</span>
+              <span>O</span>
+              <span>B</span>
+              <span>O</span>
+              <span>T</span>
+              <!--<span>I</span>
+              <span>C</span>
+              <span>A</span>-->
+            </div>
+            <div class="home__labs-type">
+              <span>R</span>
+              <span>E</span>
+              <span>D</span>
+              <span>E</span>
+              <span>S</span>
+            </div>
+          </div>
+          <div class="home__labs-lab">
+            <h4>Laboratorio de Robotica</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio nostrum deleniti doloremque. At iste doloremque provident! Et beatae dolore numquam!</p>
+            <h5>Jefe de laboratorio</h5>
+            <p>Ing. Junior Claro Pues Mijin</p>
+            <a href="#" class="home__labs-link">Ver mas</a>
+          </div>
+        </div>
+        <div class="home__labs-col">Holis</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
